18895e62559baf5395b2ff0b09612d7babc1f46b
unknownsaying <15368104568@189.cn>
2026-02-14 09:29:48 +0800

    Update UserInterface.vb

--- VirtualSpace/UserInterface.vb
+++ VirtualSpace/UserInterface.vb
@@ -2,13 +2,13 @@ Public Class VirtualSpaceForm
     Private WithEvents spaceClient As New VirtualSpaceClient()
     Private userPositions As New Dictionary(Of String, Point3D)()
     
-    Private Sub VirtualSpaceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Sub VirtualSpaceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Initialize UI
         lblStatus.Text = "Disconnected"
         btnConnect.Text = "Connect"
     End Sub
     
-    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+    Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
         If Not spaceClient.IsConnectedToSpace Then
             Dim name = InputBox("Enter your name:", "Virtual Space", spaceClient.UserName)
             If String.IsNullOrEmpty(name) Then Exit Sub
@@ -26,14 +26,14 @@ Public Class VirtualSpaceForm
         End If
     End Sub
     
-    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+    Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
         If Not String.IsNullOrEmpty(txtMessage.Text) AndAlso spaceClient.IsConnectedToSpace Then
             spaceClient.SendChatMessage(txtMessage.Text)
             txtMessage.Clear()
         End If
     End Sub
     
-    Private Sub txtMessage_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtMessage.KeyPress
+    Sub txtMessage_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtMessage.KeyPress
         If e.KeyChar = ChrW(Keys.Enter) AndAlso spaceClient.IsConnectedToSpace Then
             spaceClient.SendChatMessage(txtMessage.Text)
             txtMessage.Clear()
@@ -41,7 +41,7 @@ Public Class VirtualSpaceForm
         End If
     End Sub
     
-    Private Sub spaceClient_MessageReceived(sender As Object, message As String) Handles spaceClient.MessageReceived
+    Sub spaceClient_MessageReceived(sender As Object, message As String) Handles spaceClient.MessageReceived
         If InvokeRequired Then
             Invoke(Sub() spaceClient_MessageReceived(sender, message))
             Exit Sub
@@ -51,7 +51,7 @@ Public Class VirtualSpaceForm
         txtChat.ScrollToCaret()
     End Sub
     
-    Private Sub spaceClient_UserListUpdated(sender As Object, users As List(Of Object)) Handles spaceClient.UserListUpdated
+    Sub spaceClient_UserListUpdated(sender As Object, users As List(Of Object)) Handles spaceClient.UserListUpdated
         If InvokeRequired Then
             Invoke(Sub() spaceClient_UserListUpdated(sender, users))
             Exit Sub
@@ -65,7 +65,7 @@ Public Class VirtualSpaceForm
         Next
     End Sub
     
-    Private Sub spaceClient_ConnectionStatusChanged(sender As Object, isConnected As Boolean) Handles spaceClient.ConnectionStatusChanged
+    Sub spaceClient_ConnectionStatusChanged(sender As Object, isConnected As Boolean) Handles spaceClient.ConnectionStatusChanged
         If InvokeRequired Then
             Invoke(Sub() spaceClient_ConnectionStatusChanged(sender, isConnected))
             Exit Sub
@@ -76,23 +76,23 @@ Public Class VirtualSpaceForm
     End Sub
     
     ' Movement controls
-    Private Sub btnMoveForward_Click(sender As Object, e As EventArgs) Handles btnMoveForward.Click
+    Sub btnMoveForward_Click(sender As Object, e As EventArgs) Handles btnMoveForward.Click
         spaceClient.SendMovement(0, 1, 0)
     End Sub
     
-    Private Sub btnMoveBack_Click(sender As Object, e As EventArgs) Handles btnMoveBack.Click
+    Sub btnMoveBack_Click(sender As Object, e As EventArgs) Handles btnMoveBack.Click
         spaceClient.SendMovement(0, -1, 0)
     End Sub
     
-    Private Sub btnMoveLeft_Click(sender As Object, e As EventArgs) Handles btnMoveLeft.Click
+    Sub btnMoveLeft_Click(sender As Object, e As EventArgs) Handles btnMoveLeft.Click
         spaceClient.SendMovement(-1, 0, 0)
     End Sub
     
-    Private Sub btnMoveRight_Click(sender As Object, e As EventArgs) Handles btnMoveRight.Click
+    Sub btnMoveRight_Click(sender As Object, e As EventArgs) Handles btnMoveRight.Click
         spaceClient.SendMovement(1, 0, 0)
     End Sub
     
-    Private Sub VirtualSpaceForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+    Sub VirtualSpaceForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         spaceClient.Disconnect()
     End Sub
 End Class
@@ -101,4 +101,5 @@ Public Class Point3D
     Public Property X As Double
     Public Property Y As Double
     Public Property Z As Double
+
 End Class
